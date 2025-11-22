1bbce9a855db6b507c00ac16383dc7355daab021
José Junior <jeyjuniordg@gmail.com>
2025-11-22 17:02:50 -0300

    Add metodo para obter descricao de enums

---
+++ JJ.Net.Core/Extensoes/EnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJ.Net.Core.Extensoes
+{
+    public static class EnumExtensions
+    {
+        public static string ObterDescricao(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+
+            if (field == null)
+                return "";
+
+            var attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+
+            if (attribute == null) 
+                return "";
+
+            return attribute.Description;
+        }
+    }
+}
